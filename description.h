31c74e989af36bfc2a3ba5af83311da10b2191b5
parfemrogozin <parfem.rogozin@gmail.com>
2022-08-06 21:07:32 +0200

    arrays of pointers to feast names

--- description.h
+++ description.h
@@ -3,7 +3,7 @@ const int SECONDS_IN_WEEK = SECONDS_IN_DAY * 7;
 
 
 
-const char* const tides[] = {
+char tides[][13] = {
   "adventní",
   "vánoční",
   "po Epfanii",
@@ -12,37 +12,33 @@ const char* const tides[] = {
   "po Trojici",
   };
 
-const char* const fixed_feast_name[] = {
-  "Boží hod vánoční",
-  "Mučedníka Štěpána",
-  "Epifanie"
-
-};
-
-const char* const fixed_feast_date[] = {
+char fixed_feast_date[][6] = {
   "12-25",
   "12-26",
   "01-06"
 };
 
-const char* const easter_related_name[] = {
-  "Masopustní neděle",
-  "Popeleční středa",
-  "Květná neděle",
-  "Zelený čtvrtek",
-  "Velký pátek",
-  "Boží hod velikonoční",
-  "Nanebevstoupení Páně",
-  "Neděle po nanebevstoupení",
-  "Hod. sv. Ducha",
-  "Hod sv. Trojice",
+char feast_name[][27] = {
+  "Boží hod vánoční", //0 t f0
+  "Mučedníka Štěpána", //1 f1
+  "Epifanie", //2 t f3
+  "Masopustní neděle", //3 e0
+  "Popeleční středa", //4 t e1
+  "Květná neděle", //5 e2
+  "Zelený čtvrtek", //6 e3
+  "Velký pátek", //7  e4
+  "Boží hod velikonoční", //8 t e5
+  "Nanebevstoupení Páně", //9 e6
+  "Neděle po nanebevstoupení", //10 e7
+  "Hod. sv. Ducha", //11 e8
+  "Hod sv. Trojice" //12 t e9
 };
 
 const time_t easter_related_timedelta[] =
 {
   -7 * SECONDS_IN_WEEK,
   -46 * SECONDS_IN_DAY,
-  -SECONDS_IN_WEEK,
+  -1 * SECONDS_IN_WEEK,
   -3 * SECONDS_IN_DAY,
   -2 * SECONDS_IN_DAY,
   0,
@@ -54,10 +50,16 @@ const time_t easter_related_timedelta[] =
 
 char * starts_tide[] =
 {
-  fixed_feast_name[0],
-  fixed_feast_name[3],
-  easter_related_name[1],
-  easter_related_name[5],
-easter_related_name[9]
+  feast_name[0],
+  feast_name[2],
+  feast_name[4],
+  feast_name[8],
+  feast_name[12]
 };
 
+char * is_fixed[] =
+{
+  feast_name[0],
+  feast_name[1],
+  feast_name[2],
+};
